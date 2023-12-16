cac30f319f8288911ea0ad070ebedaec661c42d0
Jesse Suero (jesuero) <jesuero@cisco.com>
2023-12-16 10:52:49 -0500

    Drop files from .gitignore

---
+++ 2023/day-17/index.js
@@ -0,0 +1,44 @@
+import {
+  getInput,
+  formatBoard,
+  lcm,
+  Stack,
+  msToTime,
+  allEqual,
+  combination,
+  Memoizer,
+  setCharAt,
+} from "../utils/index.js";
+
+function part1(input) {
+  let count = 0;
+  // const board = formatBoard(input, true, ".");
+  //let lines = input
+  //   .split("\n")
+  //   .map((line) => line.replace(/^Card \d+: /, ""))
+  //   .forEach(function (line) {});
+  return count;
+}
+
+function part2(input) {
+  let count = 0;
+  return count;
+}
+
+const input = getInput(import.meta.url);
+let start = performance.now();
+const answer1 = part1(input);
+const part1Time = performance.now() - start;
+start = performance.now();
+const answer2 = part2(input);
+const part2Time = performance.now() - start;
+
+console.log(`
+Part 1 
+   Answer: ${answer1}
+   Time Elapsed: ${msToTime(part1Time)}
+
+Part 2 
+   Answer: ${answer2}
+   Time Elapsed: ${msToTime(part2Time)}
+`);
